777852c9271cf2508d8083d1d1b013d57560b30e
marcojalenyu <112309140+marcojalenyu@users.noreply.github.com>
2025-02-07 07:48:09 +0800

    feat: Extended timestamp to ns

--- Common.cpp
+++ Common.cpp
@@ -4,7 +4,7 @@ String Common::getTimestamp()
 {
     auto now = std::chrono::system_clock::now();
     auto now_c = std::chrono::system_clock::to_time_t(now);
-    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
+    auto ms = std::chrono::duration_cast<std::chrono::nanoseconds>(now.time_since_epoch()) % 1000000000;
 
     std::tm timeInfo;
 	localtime_s(&timeInfo, &now_c); // Thread-safe version of localtime
@@ -14,7 +14,7 @@ String Common::getTimestamp()
 	oss << std::put_time(&timeInfo, "%Y %m %d %H:%M:%S") 
         << '.' 
         << std::setfill('0') 
-        << std::setw(3) 
+        << std::setw(9) 
         << ms.count();
 
     return oss.str();
